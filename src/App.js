7d0d58be6e58b6a70c613e9ceca1c830c3a3e7df
Peyton Kellogg <pekellogg@hotmail.com>
2022-11-03 12:31:51 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem />
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>March 30, 2023</div>
+      <h2>Car Insurance</h2>
+      <div>$300</div>
+    </div>
+  );
+}
