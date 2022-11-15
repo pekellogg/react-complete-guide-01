5299da5dc53b194ad85f81fa330d952faabd6c05
Peyton Kellogg <pekellogg@hotmail.com>
2022-11-14 20:38:33 -0800

    refactored App to point to Expenses component which contains all expenses-related items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import staticExpenses from "./components/Expenses/StaticExpenses";
-import NewExpense from "./components/Expenses/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
-  const [expenses, setExpenses] = useState(staticExpenses);
-
-  const addExpenseHandler = (expense) => {
-    setExpenses((previousExpenses) => {
-      return [expense, ...previousExpenses];
-    });
-  };
-
   return (
     <div className="main">
-      <NewExpense onAdd={addExpenseHandler} />
-      <Expenses all={expenses} />
+      <Expenses />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,34 @@
 import { useState } from "react";
 import Card from "../Card";
 import ExpensesFilter from "./ExpensesFilter";
+import staticExpenses from "./StaticExpenses";
+import NewExpense from "./NewExpense";
 import ExpensesChart from "./ExpensesChart";
 import ExpensesList from "./ExpensesList";
 import "../styles/Expenses/Expenses.css";
 
 const Expenses = (props) => {
+  const [expenses, setExpenses] = useState(staticExpenses);
   const [selectedYear, setSelectedYear] = useState("");
 
   const filterChangeHandler = (year) => {
     setSelectedYear(year);
   };
 
-  const filtered = props.all.filter((expense) => {
+  const addExpenseHandler = (expense) => {
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses];
+    });
+  };
+
+  const filtered = expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === selectedYear;
   });
 
   return (
-    <div>
+    <div className="expenses">
       <Card className="expenses">
+        <NewExpense onAdd={addExpenseHandler} />
         <ExpensesFilter
           selected={selectedYear}
           onChangeFilter={filterChangeHandler}
